9a0dcdbe9282425c9833b51fb2d721181d3e0044
hafsah1976 <5677899+hafsah1976@users.noreply.github.com>
2023-05-25 17:56:39 -0400

    Create script.js
    classwork

---
+++ script.js
@@ -0,0 +1,14 @@
+//WRITE YOUR CODE BELOW
+
+//variable declaration
+var students=["Angelin", "Lucas", "John", "Jacob", "Hafsah"];
+
+//printing number of students
+console.log(students[students.length]);
+
+//printing 5 times
+
+console.log(students.length);
+for(i=0;i<5;i++){
+console.log("Great to see you, " + students[i]+ ".");
+}
